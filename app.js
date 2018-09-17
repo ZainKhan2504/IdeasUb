90edf7a6e6ee70342e75d9124e6d492413030f45
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-17 18:01:04 +0500

    Create show and add page of ideas

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-const express = require("express");
-const exphbs  = require('express-handlebars');
-const app     = express();
+const express    = require("express");
+const exphbs     = require("express-handlebars");
+const bodyParser = require("body-parser");
+const mongoose   = require("mongoose");
+
+const app      = express();
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+
+// Connect to mongoose
+mongoose
+  .connect(
+    "mongodb://localhost/ideasub",
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Load Idea Model
+require("./models/Idea");
+const Idea = mongoose.model("ideas");
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 // index route
 app.get("/", (req, res) => {
@@ -17,6 +42,50 @@ app.get("/", (req, res) => {
 // about route
 app.get("/about", (req, res) => {
     res.render("about");
+});
+
+// Idea Index Page
+app.get("/ideas", (req, res) => {
+    Idea.find({})
+        .sort({date:"desc"})
+        .then(ideas => {
+            res.render("ideas/index", {
+                ideas: ideas
+            });
+        });
+});
+
+// Add Idea Form
+app.get("/ideas/add", (req, res) => {
+  res.render("ideas/add");
+});
+
+// Submit From Data
+app.post("/ideas", (req, res) => {
+    let errors = [];
+    if (!req.body.title) {
+        errors.push({text: "Please add a title"});
+    }
+    if (!req.body.details) {
+        errors.push({ text: "Please add some details" });
+    }
+    if (errors.length > 0) {
+        res.render("ideas/add", {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else{
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect("/ideas");
+            });
+    }
 });
 
 const port = 5000;
