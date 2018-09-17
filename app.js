17c8c1ba24e509e28309fe28af8c7b8e2376018c
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-17 19:25:31 +0500

    Update and delete of idea and also added flash message

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-const express    = require("express");
-const exphbs     = require("express-handlebars");
-const bodyParser = require("body-parser");
-const mongoose   = require("mongoose");
-
-const app      = express();
+const express        = require("express");
+const exphbs         = require("express-handlebars");
+const bodyParser     = require("body-parser");
+const methodOverride = require("method-override");
+const session        = require("express-session");
+const flash          = require("connect-flash");
+const mongoose       = require("mongoose");
+const app            = express();
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -30,6 +32,27 @@ app.set('view engine', 'handlebars');
 // BodyParser Middleware
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+// Method Override Middleware
+app.use(methodOverride("_method"));
+
+// Express-Session Middleware
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Connect flash Middleware
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+})
 
 // index route
 app.get("/", (req, res) => {
@@ -83,9 +106,50 @@ app.post("/ideas", (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
+                req.flash("success_msg", "Idea Added");
                 res.redirect("/ideas");
             });
     }
+});
+
+// Edit Idea Form
+app.get("/ideas/edit/:id", (req, res) => {
+    Idea.findOne({
+       _id:req.params.id 
+    })
+    .then(idea => {
+        res.render("ideas/edit", {
+            idea: idea
+        });
+    });
+});
+
+// Edit Form Process
+app.put("/ideas/:id",(req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        // change values
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+        idea.save()
+            .then(idea => {
+                req.flash("success_msg", "Idea Updated");
+                res.redirect("/ideas");
+            });
+    });
+});
+
+// Delete Idea
+app.delete("/ideas/:id", (req, res) => {
+    Idea.deleteOne({
+        _id: req.params.id
+    })
+    .then(() => {
+        req.flash("success_msg", "Idea Removed")
+        res.redirect("/ideas");
+    });
 });
 
 const port = 5000;
