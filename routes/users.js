48fb1d038d9274f3a51ba63637302c5310717713
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-18 23:13:02 +0500

    Final Commit

--- routes/users.js
+++ routes/users.js
@@ -72,7 +72,7 @@ router.get("/login", (req, res) => {
 router.post("/login", (req, res, next) => {
     passport.authenticate("local",{
         successRedirect: "/ideas",
-        failureRedirect: "users/login",
+        failureRedirect: "/users/login",
         failureFlash: true
     })(req, res, next);
 });
