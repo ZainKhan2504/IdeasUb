6aa9c9ec9d6388b702acf3ac1cf0fadf7a189aeb
Zain Ali Khan <33342998+ZainKhan2504@users.noreply.github.com>
2018-09-20 14:24:07 +0500

    Hide File

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV == "production") {
-    module.exports = { mongoURI: "mongodb://zainkhan2504:Zainkhan_2504@ds261072.mlab.com:61072/ideasub"}
-} else{
-    module.exports = { mongoURI: "mongodb://localhost/ideasub"}
-}
